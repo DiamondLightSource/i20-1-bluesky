2a3f46e612b2965b8c4d6360c408f323b0eefce5
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2025-02-11 15:30:22 +0000

    lint

--- src/i20_1_bluesky/plans/_init__.py
+++ src/i20_1_bluesky/plans/_init__.py
@@ -1,5 +1,3 @@
-from .direct_slit_movement import continuous_movement
+from .direct_slit_movement import continuous_movement, step_scan_one_motor
 
-__all__ = [
-  "continuous_movement"
-]
+__all__ = ["continuous_movement", "step_scan_one_motor"]
